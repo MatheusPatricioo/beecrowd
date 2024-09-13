bfcf4d457c656c750063a2c8340588c316dcf626
Matheus Patricio <matheuspatricio.aspx@gmail.com>
2024-09-13 12:06:48 -0300

    Adicionando novos arquivos ao projeto

---
+++ modulo1/ex9a.php
@@ -0,0 +1,18 @@
+<?php
+// Entrada
+$T = intval(trim(fgets(STDIN))); // Tipo correto de chá
+$respostas = array_map('intval', explode(' ', trim(fgets(STDIN)))); // Respostas dos cinco competidores
+
+// Variável para contar os acertos
+$acertos = 0;
+
+// Verifica quantos competidores acertaram
+foreach ($respostas as $resposta) {
+    if ($resposta == $T) {
+        $acertos++;
+    }
+}
+
+// Saída
+echo $acertos . "\n";
+?>
