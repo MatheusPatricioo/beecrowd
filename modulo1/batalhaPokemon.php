6fd57c965e289661387e01bafc74ba5232c87262
Matheus Patricio da S e Silva <106037069+MatheusPatricioo@users.noreply.github.com>
2024-09-19 14:13:36 -0300

    Update batalhaPokemon.php

--- modulo1/batalhaPokemon.php
+++ modulo1/batalhaPokemon.php
@@ -8,27 +8,49 @@ system('cls');
 echo "Novo comando: escolha seu pokemon.\n";
 sleep(1);
 
-$pk1 = "Charmander";
-$pk2 = "Bulbasaur";
-$pk3 = "Squartle";
-$pk1 = trim($pk1);
-$pk2 = trim($pk2);
-$pk3 = trim($pk3);
-echo  "1: ". $pk1 . "\n";
-echo  "2: ".$pk2 . "\n";
-echo  "3: ".$pk3 . "\n";
+
+$pokemon1 = [$hp = 100, $atk = 30, $nome = "Charmander"];
+$pokemon2 = [$hp = 100, $atk = 30, $nome = "Bulbasaur"];
+$pokemon3 = [$hp = 100, $atk = 30, $nome = "Squartle"];
+$boss = [$hp = 120, $atk = 20, $nome = "Pikachu"];
+
+$pokemons = [$pokemon1, $pokemon2, $pokemon3];
+
+for ($i = 0; $i < count($pokemons); $i++) {
+    echo $i . " - " . $pokemons[$i][2] . "\n";
+}
 
 echo "Digite o número do Pokémon desejado: ";
 $escolha = intval(fgets(STDIN));
 
-if ($escolha == 1) {
-    echo "Você escolheu o Charmander";
-} elseif ($escolha == 2) {
-    echo "Você escolheu o Bulbasaur";
-} elseif ($escolha == 3) {
-    echo "Você escolheu o Squirtle";
+
+if ($escolha >= 0 && $escolha < count($pokemons)) {
+    $pokemonEscolhido = $pokemons[$escolha];
+    
+    sleep(1);
+    system('cls');
+    
+    echo "Você escolheu o " . $pokemonEscolhido[2] . "!\n";
+    echo "Status do Pokémon: HP = " . $pokemonEscolhido[0] . ", ATK = " . $pokemonEscolhido[1] . "\n";
 } else {
-    echo "Opção inválida.";
+    echo "Opção inválida. Por favor, escolha um número entre 0 e " . (count($pokemons) - 1) . ".\n";
 }
 
-?>
+sleep(1);
+system('cls');
+
+do {
+    echo 'Deseja batalhar? (s/n): ';
+    $batalhar = strtolower(trim(fgets(STDIN)));  // Lê a entrada e converte para minúsculas
+
+    if ($batalhar !== 's' && $batalhar !== 'n') {
+        echo "Resposta inválida. Por favor, responda com 's' para sim ou 'n' para não.\n";
+    }
+} while ($batalhar !== 's' && $batalhar !== 'n');  // Continua perguntando até receber "s" ou "n"
+
+
+if ($batalhar == 's') {
+    echo 'Prepare-se, a batalha vai começar!';
+} else {
+    echo 'Batalha cancelada.';
+}
