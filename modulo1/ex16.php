bdee13988bb9aef18f4ab9ae7bf3a57c8429755d
Matheus Patricio da S e Silva <106037069+MatheusPatricioo@users.noreply.github.com>
2024-09-30 11:38:31 -0300

    up

--- modulo1/ex16.php
+++ modulo1/ex16.php
@@ -1,24 +1,28 @@
-Problema:
-Você precisa criar um programa em PHP que peça ao usuário para inserir 6 idades de pessoas. Depois de coletar essas idades,
- o programa deve:
 
-Calcular e exibir a idade média.
-Exibir todas as idades que estão acima da idade média.
-Regras:
-Use um vetor para armazenar as idades.
-Use um laço de repetição para coletar as idades do usuário.
-Calcule a média das idades.
-Exiba as idades que estão acima da média.
 
 <?php
-    $idades =[];
-    
-    for ($i = 0; $i <= 5; $i++) {
-        echo "Digite a idade das pessoas " . ($i + 1) . ": ";
-        $idade = (int) readline();
-        $idades[] = $i;
+$idades = [];
 
-    
+for ($i = 0; $i <= 5; $i++) {
+    echo "Digite a idade das pessoas " . ($i + 1) . ": ";
+    $idade = (int) readline();
+    $idades[] = $idade;
+}
+
+$soma = array_sum($idades);
+$media = $soma / 6;
+
+
+echo "As idades digitadas foram: " . implode(", ", $idades) . "\n";
+
+echo "A média das idades é: " . $media . "\n";
+
+foreach ($idades as $idade) {
+    if ($idade > $media) {
+        echo "a idade que está acima da media: " .$idade;
     }
+}
+
+
 
 ?>
